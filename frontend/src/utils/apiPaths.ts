9dc6d93638c5a21ce7ca9f44b17d1d26f6fc9bd7
dylhead <dylhead2003@gmail.com>
2025-09-15 14:52:09 -0700

    Frontend: Changed apiPath BaseUrl to work in the docker compose

--- frontend/src/utils/apiPaths.ts
+++ frontend/src/utils/apiPaths.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = 'http://genrify-backend:5000';
 
 export const API_PATHS = {
     LOGIN: '/api/login', // Redirects to Spotify login page if the user is not logged in
